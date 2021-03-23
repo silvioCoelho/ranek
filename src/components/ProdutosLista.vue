293baf97d45af2de0fed1dfeb7808e1f70796dff
Silvio Coelho <sacoelho2@outlook.com>
2021-03-23 15:00:36 -0300

    home

--- src/components/ProdutosLista.vue
+++ src/components/ProdutosLista.vue
@@ -1,9 +1,34 @@
  <template>
-  <h1>lista</h1>
+  <section class="produtos-container">
+    <div v-for="{ id, nome, preco, descricao, fotos } in produtos" :key="id">
+      <img v-if="fotos" :src="fotos[0].src" :alt="fotos[0].titulo" />
+      <p class="preco">{{ preco }}</p>
+      <h2 class="titulo">{{ nome }}</h2>
+      <p>{{ descricao }}</p>
+    </div>
+  </section>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      produtos: null,
+    };
+  },
+  methods: {
+    getProdutos() {
+      fetch("http://localhost:3000/produto")
+        .then((r) => r.json())
+        .then((r) => {
+          this.produtos = r;
+        });
+    },
+  },
+  created() {
+    this.getProdutos();
+  },
+};
 </script>
 
 <style>
