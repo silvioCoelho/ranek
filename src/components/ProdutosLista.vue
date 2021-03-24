16ac817b00ccc5fb5040f50a31dffa9f7d9004f8
Silvio Coelho <sacoelho2@outlook.com>
2021-03-24 11:34:28 -0300

    style busca

--- src/components/ProdutosLista.vue
+++ src/components/ProdutosLista.vue
@@ -1,10 +1,23 @@
  <template>
   <section class="produtos-container">
-    <div v-for="{ id, nome, preco, descricao, fotos } in produtos" :key="id">
-      <img v-if="fotos" :src="fotos[0].src" :alt="fotos[0].titulo" />
-      <p class="preco">{{ preco }}</p>
-      <h2 class="titulo">{{ nome }}</h2>
-      <p>{{ descricao }}</p>
+    <div v-if="produtos && produtos.length" class="produtos">
+      <div
+        class="produto"
+        v-for="{ id, nome, preco, descricao, fotos } in produtos"
+        :key="id"
+      >
+        <router-link to="/">
+          <img v-if="fotos" :src="fotos[0].src" :alt="fotos[0].titulo" />
+          <p class="preco">{{ preco }}</p>
+          <h2 class="titulo">{{ nome }}</h2>
+          <p>{{ descricao }}</p>
+        </router-link>
+      </div>
+    </div>
+    <div v-else-if="produtos && produtos.length === 0">
+      <p class="sem-resultados">
+        Busca sem resultados. Tente buscar outro termo.
+      </p>
     </div>
   </section>
 </template>
@@ -45,5 +58,49 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+.produtos-container {
+  max-width: 1000px;
+  margin: 0 auto;
+}
+
+.produtos {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 30px;
+  margin: 30px;
+}
+
+.produto {
+  box-shadow: 0 4px 8px rgba(30, 60, 90, 0.1);
+  padding: 10px;
+  background: #fff;
+  border-radius: 4px;
+  transition: all 0.2s;
+}
+
+.produto:hover {
+  box-shadow: 0 6px 12px rgba(30, 60, 90, 0.12);
+  transform: scale(1.1);
+  position: relative;
+  z-index: 1;
+}
+
+.produto img {
+  border-radius: 4px;
+  margin-bottom: 20px;
+}
+
+.titulo {
+  margin-bottom: 10px;
+}
+
+.preco {
+  color: #e80;
+  font-weight: bold;
+}
+
+.sem-resultados {
+  text-align: center;
+}
 </style>
