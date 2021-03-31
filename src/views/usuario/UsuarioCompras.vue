30ec1fbd126e95cac833dd37e9dbda128aae2169
Silvio Coelho <sacoelho2@outlook.com>
2021-03-31 14:21:00 -0300

    Finalizado

--- src/views/usuario/UsuarioCompras.vue
+++ src/views/usuario/UsuarioCompras.vue
@@ -1,11 +1,70 @@
 <template>
-  <p>Usuario Compras</p>
+  <section>
+    <div v-if="compras">
+      <h2>Compras</h2>
+      <div
+        class="produtos-wrapper"
+        v-for="(compra, index) in compras"
+        :key="index"
+      >
+        <ProdutoItem v-if="compra.produto" :produto="compra.produto">
+          <p class="vendedor">
+            <span>Vendedor:</span> {{ compra.vendedor_id }}
+          </p>
+        </ProdutoItem>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-export default {};
+import ProdutoItem from "@/components/ProdutoItem.vue";
+import { api } from "@/services.js";
+import { mapState } from "vuex";
+
+export default {
+  name: "UsuarioCompras",
+  components: {
+    ProdutoItem,
+  },
+  data() {
+    return {
+      compras: null,
+    };
+  },
+  computed: {
+    ...mapState(["usuario", "login"]),
+  },
+  methods: {
+    getCompras() {
+      api.get(`/transacao?comprador_id=${this.usuario.id}`).then((r) => {
+        this.compras = r.data;
+      });
+    },
+  },
+  watch: {
+    login() {
+      this.getCompras();
+    },
+  },
+  created() {
+    if (this.login) {
+      this.getCompras();
+    }
+  },
+};
 </script>
 
-<script>
-export default {};
-</script>
+<style scoped>
+.produtos-wrapper {
+  margin-bottom: 40px;
+}
+
+.vendedor span {
+  color: #e80;
+}
+
+h2 {
+  margin-bottom: 20px;
+}
+</style>
